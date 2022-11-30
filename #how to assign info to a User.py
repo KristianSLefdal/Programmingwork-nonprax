8969348ccc80abdf64fa8c2ea242d790a4279055
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-11-30 16:47:54 +0100

    Add files via upload

---
+++ #how to assign info to a User.py
@@ -0,0 +1,7 @@
+#User1
+Value_name = "name: Kristian"
+Value_age = " Age: 21 "
+Value_school = " school: Noroff School of technology and digital media "
+Value_interest = "interest:  Football, warzone, series "
+uservalue = (Value_name + Value_age + Value_school + Value_interest)
+print (uservalue)
