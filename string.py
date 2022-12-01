cd57ca5ad450ea01bc7c0d8248d87df677b56fc8
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-12-01 12:34:03 +0100

    Add files via upload

---
+++ string.py
@@ -0,0 +1,5 @@
+first_string =" this is combined"
+second_string =" with this string to craft"
+third_string = " the most random sentence."
+sum =(first_string + second_string + third_string)
+print (sum)
