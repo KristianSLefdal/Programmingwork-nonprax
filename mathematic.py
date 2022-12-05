923e0a88905f6ab58cc61a0be044dd33b66d53e7
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-12-05 11:28:55 +0100

    Add files via upload

---
+++ mathematic.py
@@ -0,0 +1,31 @@
+#In this programming, I will go through the various mathematical methods
+#first up addition
+additon = 50 + 70
+sum = 50 + 70
+print (sum)
+#substraction
+substraction = 100 - 50
+sum = 100 - 50
+print (sum)
+#multiplication
+multiplication = 10 * 10
+sum = 10 * 10
+print (sum)
+#exponent
+#This simply adds a 0
+exponent = 10 ** 3
+sum = 10 ** 3
+print (sum)
+#division
+division = 10 / 2
+sum = 10 / 2
+print (sum)
+#floor division
+#removes all decimals after . and gives u closest to 0
+floor_division = 5 // 2
+sum = 5 // 2
+print (sum)
+#modulo divides first and last value but returns whats leftover
+modulo = 5 % 2 
+sum = 5 % 2
+print (sum)
