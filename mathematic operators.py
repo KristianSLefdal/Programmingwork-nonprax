2cd22d8afe4688685dd0cbd65f42f29b0c26995e
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-12-05 12:08:28 +0100

    Add files via upload

---
+++ mathematic operators.py
@@ -0,0 +1,21 @@
+a = 100
+a += 2 # Equivalent to a = a + 2
+print ("add and assign +=:\t\t", "a += 2 \t", "results in a =", a)
+a = 100
+a -= 2 # Equivalent to a = a - 2
+print ("subtract and assign -=:\t\t", "a -= 2 \t", "results in a =", a)
+a = 100
+a *= 2 # Equivalent to a = * 2
+print ("multiply and assign *=:\t\t", "a *= 2 \t", "results in a =", a)
+a = 100
+a **= 2 # Equivalent to a = ** 2
+print ("multiply and assign **=:\t\t", "a **= 2 \t", "results in a =", a)
+a = 100
+a /= 2 # Equivalent to a = / 2
+print ("multiply and assign /=:\t\t", "a /= 2 \t", "results in a =", a)
+a = 100
+a //= 2 # Equivalent to a = // 2
+print ("multiply and assign //=:\t\t", "a //= 2 \t", "results in a =", a)
+a = 100
+a %= 2 # Equivalent to a = % 2
+print ("multiply and assign %=:\t\t", "a %= 2 \t", "results in a =", a)
