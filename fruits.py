61ae1f79bc8d25f9c57f27b90ceba71bcc77b09a
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-12-05 12:18:33 +0100

    Add files via upload

---
+++ fruits.py
@@ -0,0 +1,4 @@
+#In this task i will assign pear to the fruit collection
+fruits = ["Banana", "pineapple", "pear", "apple", "strawberry","avocado","blueberries","cherry","grape","kiwi","lemon"]
+if "pear" in fruits
+print("yes, pear is a fruit")
