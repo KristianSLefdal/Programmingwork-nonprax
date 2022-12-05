6eaf787b0c9cc915e853ab86d6f7760d1eadfa5b
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-12-05 14:51:47 +0100

    Add files via upload

---
+++ music.py
@@ -0,0 +1,41 @@
+print("1 \ The weeknd-How do i make you love me")
+print("2 \ The weeknd-In your eyes")
+print("3 \ The weeknd-Dont break my heart")
+print("4 \ The weeknd-blinding lights")
+print("5 \ The weeknd-Moth to a flame")
+print("6 \ The weeknd-is there someone else")
+print("7 \ The weeknd-out of time")
+print("8 \ The weeknd-Less of zero")
+
+menuselection = int(input("please select a menu option :"))
+
+#checking if number is valid
+if 1<= menuselection <= 8:
+
+    #all the 8 are listed
+    if (menuselection == 1):
+        print ("1 \ The weeknd-How do i make you love me")
+
+    elif ( menuselection == 2):
+        print("2 \ The weeknd-In your eyes")
+    
+    elif (menuselection == 3):
+        print ("3 \ The weeknd-Dont break my heart")
+    
+    elif (menuselection == 4):
+        print ("4 \ The weeknd-blinding lights")
+    
+    elif (menuselection == 5):
+        print ("5 \ The weeknd-Moth to a flame")
+    
+    elif (menuselection == 6):
+        print ("6 \ The weeknd-is there someone else")
+    
+    elif (menuselection == 7):
+        print ("7 \ The weeknd-out of time")
+    
+    elif (menuselection == 8):
+        print ("8 \ The weeknd-Less of zero")
+    
+    else:
+        print (" Your choice:")
