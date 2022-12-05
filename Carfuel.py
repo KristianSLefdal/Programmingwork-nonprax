2407b5a751170be28ec6d25c20cdbe57152529da
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-12-05 13:04:35 +0100

    Add files via upload

---
+++ Carfuel.py
@@ -0,0 +1,11 @@
+fuel =9
+if fuel > 35:
+    print("drive fast")
+else:
+    if fuel > 25:
+        print("drive normally")
+    else:
+        if fuel > 15:
+            print("drive slow")
+        else:
+            print ("stop to refuel")
