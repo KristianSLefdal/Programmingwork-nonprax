9719e3ad1a43fabb3b836e254554e15adc76212f
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2023-01-13 14:18:37 +0100

    Add files via upload

---
+++ Personbook.py
@@ -0,0 +1,36 @@
+def greeting(name):
+    print("Hello, " + name)
+
+person1 = {
+    "Name": "Kristoffer",
+    "age": "27",
+    "country": "Norway"
+}
+print (person1)
+
+person2 = {
+    "Name": "Harry",
+    "age": 22,
+    "country": "UK"
+}
+print(person2)
+
+person3 = {
+    "Name": "Bob",
+    "age": 18,
+    "country": "Sweden"
+}
+print(person3)
+
+Person4 = {
+    "Name": "Charles",
+    "age": 25,
+    "country": "United States"
+}
+print(Person4)
+person5 = {
+    "Name": "Francis",
+    "age": 29,
+    "country": "France"
+}
+print(person5)
